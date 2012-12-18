55d3ebe2517c47717d0c56ffbab25f0c781b9127
Alexey Gordeyev <aleksej@gordejev.lv>
2012-12-18 14:23:07 +0200

    Fix install for CompoundJS

--- install.js
+++ install.js
@@ -4,12 +4,12 @@ var fs = require('fs')
 , repl2 = "app.use(useragent.express());    app.use(app.router);";
 
 
-if (path.existsSync(app.root + '/config/environment.js')) {
-    var data = fs.readFileSync(app.root + '/config/environment.js', 'utf8');
+if (path.existsSync(__dirname + '/config/environment.js')) {
+    var data = fs.readFileSync(__dirname + '/config/environment.js', 'utf8');
     if(!/var express = require('express');/gim.test(data) && !/app.use(app.router);/gim.test(data)) {
         data.replace(/var express = require('express');/gim,repl1)
             .replace(/app.use(app.router);/gim,repl2);
-        fs.writeFileSync(app.root + '/config/environment.js', data, 'utf8');
+        fs.writeFileSync(__dirname + '/config/environment.js', data, 'utf8');
     }
 }
 
